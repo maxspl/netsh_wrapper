e4eff3f152c7812e9d5dcd54412088011871ed72
maxspl <spellanzon.maxime@gmail.com>
2024-03-13 23:35:13 +0100

    init commit

--- src/main.rs
+++ src/main.rs
@@ -33,67 +33,6 @@ fn check_output(output: std::process::Output, message: &str) {
     }
 }
 
-fn main3() {
-    init_logging();
-
-    // Get command-line arguments
-    let args: Vec<String> = env::args().collect();
-
-    // Check if at least two arguments are provided
-    if args.len() < 4 {
-        error!("Usage: {} duration=<duration in secondes> output=<path> maxsize=<maximum size in MB>", args[0]);
-        return;
-    }
-
-    // Get the duration argument
-    let duration_arg = &args[1];
-    // Get the output argument
-    let output_arg = &args[2];
-    // Get the maxSize argument
-    let maxsize_arg = &args[3];
-
-    // Check args
-    if !duration_arg.starts_with("duration=") || !output_arg.starts_with("output=")  || !maxsize_arg.starts_with("maxsize=") {
-        error!("Usage: {} duration=<duration in secondes> output=<path> maxsize=<maximum size in MB>", args[0]);
-        return;
-    }
-
-    // Extract the duration string
-    let duration_string = &duration_arg["duration=".len()..];
-    let duration_int: u64 = duration_string.parse().unwrap();
-    // Extract the output string
-    let output_string = &output_arg["output=".len()..];
-    // Extract the maxsize string
-    let maxsize_string = &maxsize_arg["maxsize=".len()..];
-
-    // Start netsh
-    let powershell_command = format!(
-        "netsh trace start capture=yes tracefile={} maxSize={}",
-        output_string,
-        maxsize_string
-    );
-    debug!("Powershell command : {}",powershell_command);
-    let output = Command::new("powershell")
-        .args(&["-Command", &powershell_command])
-        .output() 
-        .expect("Failed to execute PowerShell command");
-
-    let mut message = "capture start. Duration : ".to_string() + duration_string ;
-    message = message + ". Maxsize : " + maxsize_string;
-    check_output(output, &message); //message allows to print the successful args
-    
-    // Sleep for 5 seconds
-    thread::sleep(Duration::from_secs(duration_int));
-
-    // Stop netsh
-    let output = Command::new("powershell")
-        .args(&["-Command", "netsh trace stop"])
-        .output()
-        .expect("Failed to execute PowerShell command");
-
-    check_output(output, "capture stop");
-
-}
 
 fn main() {
     init_logging();
